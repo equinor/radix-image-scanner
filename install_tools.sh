7fa9c514987e5c31a50d447675c31b335db95618
Nils Gustav Stråbø <nst@equinor.com>
2021-06-24 09:26:40 +0200

    store scan results in secret

--- install_tools.sh
+++ install_tools.sh
@@ -9,3 +9,7 @@ VERSION=$(
 wget https://github.com/aquasecurity/trivy/releases/download/v${VERSION}/trivy_${VERSION}_Linux-64bit.tar.gz
 tar zxvf trivy_${VERSION}_Linux-64bit.tar.gz
 mv trivy /usr/local/bin
+
+wget https://dl.k8s.io/release/v1.21.0/bin/linux/amd64/kubectl
+mv kubectl /usr/local/bin
+chmod +x /usr/local/bin/kubectl
--- scan_image.sh
+++ scan_image.sh
@@ -9,5 +9,24 @@ if test -f "${AZURE_CREDENTIALS}"; then
   fi
 fi
 
-trivy -q --timeout 20m --severity HIGH,CRITICAL ${IMAGE_PATH}
+mkdir /home/image-scanner/scan
+trivy -q i -f json --timeout 20m  ${IMAGE_PATH} |
+  jq '[.[] | .Target as $target | .Vulnerabilities | .[]? | {packageName: .PkgName, version: .InstalledVersion, target: $target, description: .Description, severity: .Severity, publishedDate: .PublishedDate, cwe: .CweIDs, cve: [.VulnerabilityID], cvss: .CVSS["nvd"].V3Score, references: .References}]' \
+  > /home/image-scanner/scan/vulnerabilities.json
+
+if [ $? -eq 0 ]
+then
+  jq 'group_by(.severity | ascii_downcase) | [.[] | {severity: .[0].severity | ascii_downcase, count: length}] | [.[] | {(.severity):.count}] | add | if . == null then {} else . end' /home/image-scanner/scan/vulnerabilities.json \
+  > /home/image-scanner/scan/aggregate.json
+
+  kubectl create configmap ${RESULT_CONFIGMAP_NAME} -n ${RESULT_CONFIGMAP_NAMESPACE} \
+  --from-file=vulnerability_list=/home/image-scanner/scan/vulnerabilities.json \
+  --from-file=vulnerability_count=/home/image-scanner/scan/aggregate.json \
+  --from-literal=scan_success=true
+else
+  kubectl create configmap ${RESULT_CONFIGMAP_NAME} -n ${RESULT_CONFIGMAP_NAMESPACE} \
+  --from-literal=scan_success=false
+fi
+
 exit 0
+
