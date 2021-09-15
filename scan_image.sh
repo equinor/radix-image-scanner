39b1bda6edd816f5534eb5468a7e60e89521a8c0
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2021-09-15 15:03:52 +0200

    reduce size of vulnerability list (#11)
    * reduce size of vulnerability list
    
    * remove duplicate regex
    
    Co-authored-by: Nils Gustav Stråbø <nst@equinor.com>

--- scan_image.sh
+++ scan_image.sh
@@ -38,12 +38,18 @@ fi
 mkdir /home/image-scanner/scan
 
 snyk container test --username=${REGISTRY_USERNAME} --password=${REGISTRY_PASSWORD} --json ${IMAGE_PATH} |
-  jq '[.path as $target | .vulnerabilities | .[]? | {packageName: .packageName, version: .version, target: $target, title: .title, description: .description, severity: .severity | ascii_upcase, publishedDate: .publicationTime, cwe: .identifiers.CWE, cve: .identifiers.CVE, cvss: .cvssScore, references: [.references | .[]? | .url]}]' \
+  jq '"^## NVD Description\n(.*?)\n(<i>.*</i>?)(\n<i> See `Remediation`(.*?)){0,1}\n\n(.*?)\n" as $regex | [.path as $target | .vulnerabilities | .[]? | (.description | test($regex)) as $match | {packageName: .packageName, version: .version, target: $target, title: .title, description: (if $match == false then .description else (.description | match($regex)).captures[4].string end ), severity: .severity | ascii_upcase, publishedDate: .publicationTime, cwe: .identifiers.CWE, cve: .identifiers.CVE, cvss: .cvssScore, references: ["https://snyk.io/vuln/" + .id]}]' \
   > /home/image-scanner/scan/vulnerabilities.json || exit
 
 jq 'group_by(.severity | ascii_downcase) | [.[] | {severity: .[0].severity | ascii_downcase, count: length}] | [.[] | {(.severity):.count}] | add | if . == null then {} else . end' /home/image-scanner/scan/vulnerabilities.json \
   > /home/image-scanner/scan/aggregate.json || exit
 
+if [[ $(find /home/image-scanner/scan/vulnerabilities.json -type f -size +1040000c 2>/dev/null) ]]; then
+    echo "Output from vulnerability scan exceeds 1MB and cannot be stored"
+    # Clear content of vulnerabilities file when file exceeds 1MB
+    # We set limit to 1.400.000 bytes to leave aprx 8KB to aggregated values
+    echo "[]" > /home/image-scanner/scan/vulnerabilities.json
+fi
 
 kubectl create configmap ${OUTPUT_CONFIGMAP_NAME} -n ${OUTPUT_CONFIGMAP_NAMESPACE} \
   --from-file="$VULNERABILITY_LIST_KEY"=/home/image-scanner/scan/vulnerabilities.json \
